4f63fe32421c04c9a18c593ab86c5949fb06de5b
luciano <git@github.com:lucianocanuto/TrailersPlay.git>
2025-12-08 17:02:14 -0300

    feat: tela usuario configurada

--- app/src/main/java/com/btcodans/bootstore/ui/UsuariosActivity.kt
+++ app/src/main/java/com/btcodans/bootstore/ui/UsuariosActivity.kt
@@ -2,14 +2,18 @@ package com.btcodans.bootstore.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.btcodans.bootstore.R
 import com.btcodans.bootstore.data.api.RetrofitConect
+import com.btcodans.bootstore.data.models.Usuario
 import com.btcodans.bootstore.databinding.ActivityUsuariosBinding
 import com.btcodans.bootstore.logo.BootStoreLogo
+import com.btcodans.bootstore.ui.adapters.UsuariosAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -41,8 +45,13 @@ class UsuariosActivity : AppCompatActivity() {
 
     }
 
-    private fun CoroutineScope.configuraRecyclerView() {
-        TODO("Not yet implemented")
+    private fun configuraRecyclerView(list: List<Usuario>) {
+        binding.RecyclerUser.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        binding.RecyclerUser.adapter = UsuariosAdapter(list){ usuario ->
+            enviarDetalhes(usuario)
+
+        }
     }
 
     private fun buscarUsuarios() {
@@ -50,9 +59,13 @@ class UsuariosActivity : AppCompatActivity() {
             try {
                 val buscarUsuarios = RetrofitConect.api.listarUsuarios()
 
+                Log.d("API", "Total: ${buscarUsuarios.users}, User: ${buscarUsuarios.users.size} ")
+                Log.d("API", "Primeiro usuário: ${buscarUsuarios.users.firstOrNull()}")
+
+
                 val listaUsuario = buscarUsuarios.users
                 withContext(Dispatchers.Main){
-                    configuraRecyclerView(/*listaUsuario*/)
+                    configuraRecyclerView(listaUsuario)
                 }
 
             }catch (e: Exception) {
@@ -62,3 +75,9 @@ class UsuariosActivity : AppCompatActivity() {
         }
     }
 }
+
+private fun enviarDetalhes(usuario: Usuario) {
+    TODO("Not yet implemented")
+}
+
+
--- app/src/main/java/com/btcodans/bootstore/ui/adapters/UsuariosAdapter.kt
+++ app/src/main/java/com/btcodans/bootstore/ui/adapters/UsuariosAdapter.kt
@@ -1,4 +1,44 @@
 package com.btcodans.bootstore.ui.adapters
 
-class UsuariosAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.btcodans.bootstore.data.models.Usuario
+import com.btcodans.bootstore.databinding.ItemUsuariosBinding
+import com.bumptech.glide.Glide
+
+class UsuariosAdapter (
+    private val lista: List<Usuario>,
+    private val onClick: (Usuario) -> Unit
+): RecyclerView.Adapter<UsuariosAdapter.UsuariosViewHolder>() {
+
+    inner class UsuariosViewHolder(val binding : ItemUsuariosBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+        //Vicular intens no recycler
+        fun vivculadora (usuario: Usuario){
+            Glide.with(binding.imgUser.context)
+                .load(usuario.image)
+                .into(binding.imgUser)
+            binding.txtUserNome.text = usuario.firstName
+
+            binding.root.setOnClickListener { onClick(usuario) }
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        UsuariosViewHolder (ItemUsuariosBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+    )
+
+    override fun onBindViewHolder(
+        holder: UsuariosViewHolder,
+        position: Int
+    ) {
+        holder.vivculadora(lista[position])
+    }
+
+    override fun getItemCount(): Int = lista.size
+
+
 }
