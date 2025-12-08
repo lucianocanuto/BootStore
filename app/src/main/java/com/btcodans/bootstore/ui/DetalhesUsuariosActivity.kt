0481d70b2822650724bb03abcd029854e457a4b3
luciano <git@github.com:lucianocanuto/TrailersPlay.git>
2025-12-08 19:58:40 -0300

    feat: detalhes Usuarios configurada e funcionando

--- app/src/main/java/com/btcodans/bootstore/ui/DetalhesUsuariosActivity.kt
+++ app/src/main/java/com/btcodans/bootstore/ui/DetalhesUsuariosActivity.kt
@@ -6,16 +6,48 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.btcodans.bootstore.R
+import com.btcodans.bootstore.databinding.ActivityDetalhesUsuariosBinding
+import com.btcodans.bootstore.logo.BootStoreLogo
+import com.bumptech.glide.Glide
 
 class DetalhesUsuariosActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityDetalhesUsuariosBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalhes_usuarios)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        binding.txtLogo.text = BootStoreLogo()
+
+
+        //Recebendo dados
+        val id = intent.getIntExtra("UserId",0)
+        val UserNome = intent.getStringExtra("UserNome") ?: ""
+        val UserSobreNome = intent.getStringExtra("UserSobreNome") ?: ""
+        val UserIdade = intent.getIntExtra("UserIdade",0)
+        val Usersexo = intent.getStringExtra("Usersexo")  ?: ""
+        val UserEmail = intent.getStringExtra("UserEmail")  ?: ""
+        val UserOlhos = intent.getStringExtra("UserOlhos")  ?: ""
+        val UserImagem = intent.getStringExtra("UserImagem")  ?: ""
+
+
+        Glide.with(binding.posterImageView.context)
+            .load(UserImagem)
+            .into(binding.posterImageView)
+
+        binding.textUserNome.text = UserNome
+        binding.textUserSobreNome.text = UserSobreNome
+        binding.textUserIdade.text = UserIdade.toString()
+        binding.textUserSexo.text = Usersexo
+        binding.textUserEmail.text = UserEmail
+        binding.textUserOlhos.text = UserOlhos
+
+
+
+
+
+
     }
 }
