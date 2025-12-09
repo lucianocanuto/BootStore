908468f470b9c41f442283ee068cf799ad9ef2d5
luciano <git@github.com:lucianocanuto/TrailersPlay.git>
2025-12-09 19:39:44 -0300

    feat: Postagem DONE.

--- app/src/main/java/com/btcodans/bootstore/data/models/Postagem.kt
+++ app/src/main/java/com/btcodans/bootstore/data/models/Postagem.kt
@@ -1,8 +1,12 @@
 package com.btcodans.bootstore.data.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Postagem(
     val id: Int,
     val title: String,
     val body: String,
     val userId: Int
-)
+) : Parcelable
--- app/src/main/java/com/btcodans/bootstore/ui/adapters/PostagensAdapter.kt
+++ app/src/main/java/com/btcodans/bootstore/ui/adapters/PostagensAdapter.kt
@@ -39,7 +39,7 @@ class PostagensAdapter(
 
             binding.root.setOnClickListener { onClick (postagem) }
         }
-        }
+    }
 
 
 }
