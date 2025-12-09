01a4c123ce7f5ac61a6f2a794b0f2deadc7254f6
luciano <git@github.com:lucianocanuto/TrailersPlay.git>
2025-12-09 17:19:36 -0300

    feat: Criado item postagem

--- app/src/main/java/com/btcodans/bootstore/ui/PostagensActivity.kt
+++ app/src/main/java/com/btcodans/bootstore/ui/PostagensActivity.kt
@@ -6,16 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.btcodans.bootstore.R
+import com.btcodans.bootstore.databinding.ActivityPostagensBinding
+import com.btcodans.bootstore.logo.BootStoreLogo
 
 class PostagensActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPostagensBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_postagens)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+        binding.txtLogo.text = BootStoreLogo()
+
     }
 }
